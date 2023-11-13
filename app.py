c37b424527d78fbde6a5d84e0c48f9bb817147ef
amit shochat <48761476+amit-shochat@users.noreply.github.com>
2023-11-13 13:11:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
-
+import logging
+import sys
 import os
 from logstash_async.handler import AsynchronousLogstashHandler
 from logstash_async.formatter import FlaskLogstashFormatter
 
-# LOGSTASH_HOST = os.environ.get('LOGSTASH_HOST', None)
-# LOGSTASH_DB_PATH = os.environ.get('LOGSTASH_DB_PATH', None)
-# LOGSTASH_TRANSPORT = os.environ.get('LOGSTASH_TRANSPORT', None)
-# LOGSTASH_PORT = os.environ.get('LOGSTASH_PORT', None)
+app = Flask(__name__)
 
-LOGSTASH_HOST="192.168.122.29"
-LOGSTASH_DB_PATH="/python-app"
-LOGSTASH_TRANSPORT="logstash_async.transport.BeatsTransport"
-LOGSTASH_PORT="5044"
+LOGSTASH_HOST = os.environ.get('LOGSTASH_HOST', None)
+LOGSTASH_DB_PATH = os.environ.get('LOGSTASH_DB_PATH', None)
+LOGSTASH_TRANSPORT = os.environ.get('LOGSTASH_TRANSPORT', None)
+LOGSTASH_PORT = os.environ.get('LOGSTASH_PORT', None)
 
-logstash_handler = AsynchronousLogstashHandler(
-    LOGSTASH_HOST,
-    LOGSTASH_PORT,
-    database_path=LOGSTASH_DB_PATH,
-    transport=LOGSTASH_TRANSPORT,
-)
+logger = logging.getLogger("Flask & Docker & Logstah ( Fuck that shit)")
+logger.setLevel(logging.INFO)
+logger.addHandler(AsynchronousLogstashHandler(LOGSTASH_HOST, LOGSTASH_PORT, LOGSTASH_DB_PATH, LOGSTASH_TRANSPORT))
 
-logstash_handler.formatter = FlaskLogstashFormatter(metadata={"amit": "flaskapp"})
-app.logger.addHandler(logstash_handler)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello from Flask & Docker</h2>'
-
+def home():
+    logger.info("Hello from Flask & Docker & Logstah")
+    return '<h1>Hello from Flask & Docker & Logstah</h1>'
 if __name__ == "__main__":
     app.run(debug=True)
