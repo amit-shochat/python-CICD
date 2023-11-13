216fc517c023392cd6650ab984772161efc37249
amit shochat <48761476+amit-shochat@users.noreply.github.com>
2023-11-13 16:28:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,11 @@ LOGSTASH_HOST = os.environ.get('LOGSTASH_HOST', None)
 LOGSTASH_DB_PATH = os.environ.get('LOGSTASH_DB_PATH', None)
 LOGSTASH_TRANSPORT = os.environ.get('LOGSTASH_TRANSPORT', None)
 LOGSTASH_PORT = os.environ.get('LOGSTASH_PORT', None)
-PORT = int(LOGSTASH_PORT)
 
 logger = logging.getLogger("Flask & Docker & Logstah ( Fuck that shit)")
 logger.setLevel(logging.INFO)
 logger.formatter = FlaskLogstashFormatter(metadata={"amit": "flask-app"})
-logger.addHandler(AsynchronousLogstashHandler(LOGSTASH_HOST, PORT, LOGSTASH_DB_PATH, LOGSTASH_TRANSPORT))
+logger.addHandler(AsynchronousLogstashHandler(LOGSTASH_HOST, LOGSTASH_PORT, LOGSTASH_DB_PATH, LOGSTASH_TRANSPORT))
 
 
 @app.route('/')
